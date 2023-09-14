f89544698cff4b58fae8acf42250598e9843bf82
miskecpro <monty.green97@gmail.com>
2023-09-14 18:39:46 +0200

    Set up languages packages + added setting menu

--- app/src/main/java/com/example/skiserbia/main/MainActivity2.kt
+++ app/src/main/java/com/example/skiserbia/main/MainActivity2.kt
@@ -1,8 +1,14 @@
 package com.example.skiserbia.main
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.core.view.WindowCompat
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
@@ -10,7 +16,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.skiserbia.R
 import com.example.skiserbia.databinding.ActivityMainBinding
 
-class MainActivity2 : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() , MenuProvider {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
@@ -18,6 +24,9 @@ class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
+
+        val menuHost: MenuHost = this
+        menuHost.addMenuProvider(this, this, Lifecycle.State.RESUMED)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -31,5 +40,17 @@ class MainActivity2 : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.settings_menu, menu)
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        when (menuItem.itemId) {
+            R.id.action_settings -> true
+        }
+        // findNavController().navigate(NavigationGraphDirections.actionGlobalPremiumFragment())
+        return false
     }
 }
