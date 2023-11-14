a16982a4be6b2daec0f65782566255ae9fb17a71
miskecpro <monty.green97@gmail.com>
2023-11-14 16:10:38 +0100

    Moved weather fetching details to the ski center info fragment, amended weather info fragment

--- app/src/main/java/com/example/skiserbia/features/skicenter/SkiCenterDetailsFragment.kt
+++ app/src/main/java/com/example/skiserbia/features/skicenter/SkiCenterDetailsFragment.kt
@@ -1,6 +1,7 @@
 package com.example.skiserbia.features.skicenter
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,14 +9,26 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.skiserbia.NavigationGraphDirections
-import com.example.skiserbia.common.PreferenceProvider
+import com.example.skiserbia.common.WebScarpingServiceImpl
 import com.example.skiserbia.databinding.FragmentSkiCenterDetailsBinding
+import com.example.skiserbia.features.skicenter.weather.ForecastDay
+import com.example.skiserbia.features.skicenter.weather.WeatherInfo
+import okhttp3.ResponseBody
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SkiCenterDetailsFragment : Fragment() {
 
     private var bindingProp: FragmentSkiCenterDetailsBinding? = null
     private val binding get() = bindingProp!!
     private val skiCenterUrl: SkiCenterDetailsFragmentArgs by navArgs()
+    lateinit var temperature : String
+    lateinit var wind : String
+    lateinit var snow : String
+    lateinit var forecast : String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,12 +45,84 @@ class SkiCenterDetailsFragment : Fragment() {
         }
 
         binding.cardViewMap.setOnClickListener {
-            findNavController().navigate(NavigationGraphDirections.actionSkiMap())
+            findNavController().navigate(NavigationGraphDirections.actionSkiMap(skiCenterUrl.skiCenter))
         }
 
         binding.button2.setOnClickListener {
-            findNavController().navigate(NavigationGraphDirections.actionWeatherInfo(skiCenterUrl.skiCenter, PreferenceProvider.weatherUrl))
+            findNavController().navigate(NavigationGraphDirections.actionWeatherInfo(temperature, wind, snow, forecast))
         }
+
+
+        val call = getCall(skiCenterUrl.skiCenter)
+        call.enqueue(object : Callback<ResponseBody> {
+            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+                if (response.isSuccessful) {
+                    val responseBody = response.body()
+                    if (responseBody != null) {
+                        val htmlContent = responseBody.string()
+                        val weatherDetailsList = parseHtmlToWeatherInfo(htmlContent)
+                        temperature = weatherDetailsList.temperature
+                        wind = weatherDetailsList.windSpeed
+                        snow = weatherDetailsList.snowHeight
+                        forecast = weatherDetailsList.toString()
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                t.printStackTrace()
+            }
+        })
+
         return binding.root
+    }
+
+
+    private fun parseHtmlToWeatherInfo(html: String): WeatherInfo {
+        val document: Document = Jsoup.parse(html)
+        Log.d("Nesto", html)
+
+        val location = document.select(".current-weather .forecast-day").text()
+        val temperature = document.select(".current-weather .current-temp").text()
+        val humidityTempElements = document.select(".current-weather .humidity-temp")
+
+        var snowHeight = ""
+        var windSpeed = ""
+
+        for (i in humidityTempElements.indices) {
+            val element = humidityTempElements[i]
+            println("Humidity Temp Element: ${element.text()}")
+
+            val imgSrc = element.select("img").attr("src")
+            when {
+                imgSrc.contains("snow.png") -> snowHeight = humidityTempElements.getOrNull(i)?.text() ?: ""
+                imgSrc.contains("wind.png") -> windSpeed = humidityTempElements.getOrNull(i)?.text() ?: ""
+            }
+        }
+        val forecastDays = mutableListOf<ForecastDay>()
+
+        val forecastElements = document.select(".current-forecast")
+        for (forecastElement in forecastElements) {
+            val day = forecastElement.select(".forecast-left-side .forecast-day").text()
+            val date = forecastElement.select(".forecast-left-side .forecast-date").text()
+            val maxTemp = forecastElement.select(".forecast-right-side .forecast-temp-red").text()
+            val minTemp = forecastElement.select(".forecast-right-side .forecast-temp-blue").text()
+            val forecastWindSpeed = forecastElement.select(".forecast-right-side .forecast-cond").text()
+
+            val forecastDay = ForecastDay(day, date, maxTemp, minTemp, forecastWindSpeed)
+            forecastDays.add(forecastDay)
+        }
+
+        return WeatherInfo(location, temperature, snowHeight, windSpeed, forecastDays)
+    }
+
+    private fun getCall(skiCenterUrl: String): Call<ResponseBody> {
+        return if (skiCenterUrl.contains("kopaonik")) {
+            WebScarpingServiceImpl.getService().scrapeKopaonikWeatherWebPage()
+        } else if (skiCenterUrl.contains("tornik")) {
+            WebScarpingServiceImpl.getService().scrapeTornikWeatherWebPage()
+        } else {
+            WebScarpingServiceImpl.getService().scrapeStaraPlaninaWeatherWebPage()
+        }
     }
 }
--- app/src/main/java/com/example/skiserbia/features/skicenter/weather/WeatherFragment.kt
+++ app/src/main/java/com/example/skiserbia/features/skicenter/weather/WeatherFragment.kt
@@ -1,93 +1,32 @@
 package com.example.skiserbia.features.skicenter.weather
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
-import com.example.skiserbia.common.WebScarpingServiceImpl
 import com.example.skiserbia.databinding.FragmentWeatherInfoBinding
-import com.example.skiserbia.features.skicenter.lifts.LiftInfoFragmentArgs
-import okhttp3.ResponseBody
-import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class WeatherFragment : Fragment() {
 
     private var bindingProp: FragmentWeatherInfoBinding? = null
     private val binding get() = bindingProp!!
 
-    private val skiCenterUrl: LiftInfoFragmentArgs by navArgs()
-    private val weatherUrl: LiftInfoFragmentArgs by navArgs()
+    private val temperature: WeatherFragmentArgs by navArgs()
+    private val wind: WeatherFragmentArgs by navArgs()
+    private val snow: WeatherFragmentArgs by navArgs()
+    private val forecast: WeatherFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         bindingProp = FragmentWeatherInfoBinding.inflate(inflater, container, false)
-
-        val call = getCall(skiCenterUrl.skiCenter)
-        call.enqueue(object : Callback<ResponseBody> {
-            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
-                if (response.isSuccessful) {
-                    val responseBody = response.body()
-                    if (responseBody != null) {
-                        val htmlContent = responseBody.string()
-                        val skiLiftDetailsList = parseHtmlToWeatherInfo(htmlContent)
-                        binding.weather.text = skiLiftDetailsList.toString()
-                    }
-                }
-            }
-
-            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
-                t.printStackTrace()
-            }
-        })
-
-
+        binding.weather.text = forecast.toString()
+        binding.snowValue.text = snow.snow
+        binding.windValue.text = wind.wind
+        binding.temperatureValue.text = temperature.temperature
         return binding.root
-    }
-
-
-    private fun parseHtmlToWeatherInfo(html: String): WeatherInfo {
-        val document: Document = Jsoup.parse(html)
-        Log.d("Nesto", html)
-
-        val location = document.select(".current-weather .forecast-day").text()
-        val temperature = document.select(".current-weather .current-temp").text()
-        val snowHeight = document.select(".current-weather .humidity-temp:contains(Visina snega) + div").text()
-        val windSpeed = document.select(".current-weather .humidity-temp:contains(Brzina vetra) + div").text()
-
-        val forecastDays = mutableListOf<ForecastDay>()
-
-        val forecastElements = document.select(".current-forecast")
-        for (forecastElement in forecastElements) {
-            val day = forecastElement.select(".forecast-left-side .forecast-day").text()
-            val date = forecastElement.select(".forecast-left-side .forecast-date").text()
-            val maxTemp = forecastElement.select(".forecast-right-side .forecast-temp-red").text()
-            val minTemp = forecastElement.select(".forecast-right-side .forecast-temp-blue").text()
-            val forecastWindSpeed = forecastElement.select(".forecast-right-side .forecast-cond").text()
-
-            val forecastDay = ForecastDay(day, date, maxTemp, minTemp, forecastWindSpeed)
-            forecastDays.add(forecastDay)
-        }
-
-        return WeatherInfo(location, temperature, snowHeight, windSpeed, forecastDays)
-    }
-
-    private fun getCall(skiCenterUrl: String): Call<ResponseBody> {
-        Log.d("Nesto1", skiCenterUrl )
-        return if (skiCenterUrl.contains("kopaonik")) {
-            WebScarpingServiceImpl.getService().scrapeKopaonikWeatherWebPage()
-        } else if (skiCenterUrl.contains("tornik")) {
-            WebScarpingServiceImpl.getService().scrapeTornikWeatherWebPage()
-        } else {
-            WebScarpingServiceImpl.getService().scrapeStaraPlaninaWeatherWebPage()
-        }
     }
 }
